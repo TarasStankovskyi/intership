7e3c1e14d58ba15743188c55451c704e8507b003
TarasStankovskyi <stankovskytaras@gmail.com>
2016-12-13 10:56:49 +0200

    refactor sys script

--- sys_script.py
+++ sys_script.py
@@ -1,109 +1,99 @@
-import os
-import psutil
-from datetime import datetime
-import optparse
-import sys
+""" Script for gathering host information """
 
-def info():
+
+from datetime import datetime
+import sys
+import os
+import optparse
+import subprocess
+
+def ram():
+    c1 = ['free', '-m']
+    p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
+    c2 = ['grep', 'Mem']
+    p2 = subprocess.Popen(c2, stdin=p1.stdout, stdout=subprocess.PIPE)
+    c3 = ['awk', '{print $4/$2*100}']
+    p3 = subprocess.Popen(c3, stdin=p2.stdout, stdout=subprocess.PIPE)
+    result = p3.stdout.read()
+    return result
+
+def cpu():
+    c1 = ['top', '-b', '-n1']
+    p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
+    c2 = ['grep', 'Cpu(s)']
+    p2 = subprocess.Popen(c2, stdin=p1.stdout, stdout=subprocess.PIPE)
+    c3 = ['awk', '{print $2+$4}']
+    p3 = subprocess.Popen(c3, stdin=p2.stdout, stdout=subprocess.PIPE)
+    result = p3.stdout.read()
+    return result
+
+def cur_processes():
+    c1 = ['ps', '-c']
+    p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
+    c2 = ['grep', '-v', 'PID']
+    p2 = subprocess.Popen(c2, stdin=p1.stdout, stdout=subprocess.PIPE)
+    c3 = ['wc', '-l']
+    p3 = subprocess.Popen(c3, stdin=p2.stdout, stdout=subprocess.PIPE)
+    result = p3.stdout.read()
+    return result
+
+def main():
+    """ Function provide information about CPU, RAM usage and processes running  """
     usage = "Usage: %prog -s path to file -a append iformation to existing file "
     parser = optparse.OptionParser(usage=usage)
 
-    parser.add_option('-c', '--cpu', dest='cpu', help='Print current CPU usage', default=False, action='store_true')
-    parser.add_option('-r', '--ram', dest='ram', help='Print current RAM usage', default=False, action='store_true')
-    parser.add_option('-p', '--processes', dest='proc', help='Print current number of processes running', default=False, action='store_true')
-    parser.add_option('-a', '--append', dest='app', help='Append current info to existing file',  default=False, action='store_true')
-    parser.add_option('-s', '--file', dest ='filename', help='Path to file', default=False)
-    
+    parser.add_option('-c', '--cpu', dest='cpu',
+                      help='Print current CPU usage', default=False, action='store_true')
+    parser.add_option('-r', '--ram', dest='ram',
+                      help='Print current RAM usage', default=False, action='store_true')
+    parser.add_option('-p', '--processes', dest='proc',
+                      help='Print current number of processes running',
+                      default=False, action='store_true')
+    parser.add_option('-a', '--append', dest='app',
+                      help='Append current info to existing file',
+                      default=False, action='store_true')
+    parser.add_option('-s', '--file', dest='filename', help='Path to file', default=False)
+
     options, _ = parser.parse_args()
 
-    cur_ram = psutil.virtual_memory()
-    cur_cpu = psutil.cpu_percent(1)
-    processes = len(psutil.pids())
+    cur_ram = ram()
+    cur_cpu = cpu()
+    processes = cur_processes()
     time = datetime.now()
 
-    cpu = options.cpu
-    ram = options.ram
-    proc = options.proc
+    print cur_cpu
+
+    count = 0
+    stdout = str(time)
+
+    if options.cpu:
+        count += 1
+        stdout += '\tCPU usage: ' + str(cur_cpu)
+    if options.ram:
+        count += 1
+        stdout += '\tRAM usage: ' + str(cur_ram)
+    if options.proc:
+        count += 1
+        stdout += '\tProcesses running: ' + str(processes)
+    if count > 0:
+        print stdout +'\n'
+    else:
+        print '{0}\tCPU usage:{1}\tCurrent number of processes running'\
+              ':{2}\tRAM usage :{3}\n'.format(time, cur_cpu, processes, cur_ram)
+
     filename = options.filename
     app = options.app
 
-    if os.path.isfile(filename) and app:
+    if filename and os.path.isfile(filename) or app:
         with open(filename, 'a') as res:
-            if cpu and proc:
-                res.write('<{0}>  <Current CPU : {1}> <Current number of processes running : {2}> <Current RAM : {3}>\n'.format(time, cpu,processes, ram))
-            
-            elif cpu and ram:
-                res.write('<{0}>  <Current CPU : {1}> <Current RAM : {2}>\n'.format(time, cpu, ram))
-            
-            elif proc and ram:
-                res.write('<{0}>  <Current number of processes running : {1}> <Current RAM : {2}>\n'.format(time, processes, cur_ram))
-            
-            elif cpu:
-                res.write('<{0}>  <Current CPU : {1}>\n'.format(time, cur_cpu))
-            
-            elif ram:
-                res.write('<{0}>  <Current RAM : {1}>\n'.format(time, cur_ram))
-            
-            elif proc:
-                res.write('<{0}>  <Current number of processes running : {1}>\n'.format(time, processes))
-             
+            if count > 0:
+                res.write(stdout + '\n')
             else:
-                res.write('<{0}>  <Current CPU : {1}> <Current number of processes running : {2}> <Current RAM : {3}>\n'.format(time, cur_cpu,processes, cur_ram))
-
-    elif filename and not app:
-        with open(filename, 'w') as res:
-            if cpu and proc:
-                res.write('<{0}>  <Current CPU : {1}> <Current number of processes running : {2}> <Current RAM : {3}>\n'.format(time, cpu,processes, ram))
-
-            elif cpu and ram:
-                res.write('<{0}>  <Current CPU : {1}> <Current RAM : {2}>\n'.format(time, cpu, ram))
-            
-            elif proc and ram:
-                res.write('<{0}>  <Current number of processes running : {1}> <Current RAM : {2}>\n'.format(time, processes, cur_ram))
-
-            elif cpu:
-                res.write('<{0}>  <Current CPU : {1}>\n'.format(time, cur_cpu))
-
-            elif ram:
-                res.write('<{0}>  <Current RAM : {1}>\n'.format(time, cur_ram))
-
-            elif proc:
-                res.write('<{0}>  <Current number of processes running : {1}>\n'.format(time, processes))
-
-            else:
-                res.write('<{0}>  <Current CPU : {1}> <Current number of processes running : {2}> <Current RAM : {3}>\n'.format(time, cur_cpu,processes, cur_ram))
-
-    if cpu and proc:
-        print '<{0}>  <Current CPU : {1}> <Current number of processes running : {2}>\n'.format(time, cur_cpu, processes)
-        return 1
-
-    elif cpu and ram:
-        print '<{0}>  <Current CPU : {1}>  <Current RAM : {2}>\n'.format(time, cur_cpu, cur_ram)
-        return 1
-
-    elif proc and ram:
-        print '<{0}>  <Current number of processes running : {1}> <Current RAM : {2}>\n'.format(time, processes, cur_ram)
-        return 1
-
-    elif cpu:
-        print '<{0}>  <Current CPU : {1}>\n'.format(time, cur_cpu)
-        return 1
-
-    elif ram:
-        print '<{0}>  <Current RAM : {1}>\n'.format(time, cur_ram)
-        return 1
-
-    elif proc:
-        print '<{0}>  <Current number of processes running : {1}>\n'.format(time, processes)
-        return 1
-
-    else:
-        print '<{0}>  <Current CPU : {1}> <Current number of processes running : {2}> <Current RAM : {3}>\n'.format(time, cur_cpu,processes, cur_ram)
-        return 1
+                res.write('{0}\tCPU usage:{1}\tCurrent number of processes running'\
+                          ':{2}\t RAM usage :{3}\n'.format(time, cur_cpu, processes, cur_ram))
 
 
-    
 if __name__ == '__main__':
-    info()
+    sys.exit(main())
 
 
