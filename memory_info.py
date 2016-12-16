13b5514743bd51aa438bfd18ffec7874ba62997a
TarasStankovskyi <stankovskytaras@gmail.com>
2016-12-16 15:48:27 +0200

    refactor code

--- memory_info.py
+++ memory_info.py
@@ -23,58 +23,52 @@ def get_optparse():
     parser.add_option('-s', '--file', dest='filename', help='Path to file', default=None)
 
     options, _ = parser.parse_args()
-    option_dict = vars(options)
-    return option_dict
+    return vars(options)
 
-def sys_call(options):
+def sys_call(option):
     """Function for getting system information"""
-    option_list = ['cpu', 'ram', 'proc']
-    result = {}
-    args = []
     commands = {'ram':"free -m | grep Mem | awk '{print $4/$2*100}'",\
                 'cpu':"top -b -n1 | grep 'Cpu(s)' | awk '{print $2+$4}'",\
                 'proc':"ps -c | grep -v 'PID'| wc -l"}
 
-    del options['filename']
-    [args.append(option) for option in options if options[option]]
-    if len(args) == 0:
-        args.extend(option_list)
-    for arg in args:
-        if arg in commands:
-            result[arg] = subprocess.Popen(commands[arg], shell=True,\
-            stdout=subprocess.PIPE).stdout.read()
-    return result
+    return subprocess.Popen(commands[option], shell=True,\
+           stdout=subprocess.PIPE).stdout.read()
 
 def get_output(params):
     """Function for result returning"""
     time = t.strftime("%Y-%m-%d %H:%M:%S")
     result = [time]
     if 'cpu' in params:
-        result.append(' CPU usage: {}'.format(params['cpu']))
+        result.append('  CPU usage: {}'.format(params['cpu']))
     if 'ram' in params:
-        result.append(' RAM usage: {}'.format(params['ram']))
+        result.append('  RAM usage: {}'.format(params['ram']))
     if 'proc' in params:
-        result.append(' Processes running: {}'.format(params['proc']))
+        result.append('  Processes running: {}'.format(params['proc']))
     if len(result) == 1:
-        result.append('CPU usage:{}\tCurrent number of processes running'\
-                      ':{}\tRAM usage :{}\n'.format(params['cpu'],\
+        result.append('  CPU usage: {}  Current number of processes running'\
+                      ': {}\tRAM usage: {}\n'.format(params['cpu'],\
                       params['proc'], params['ram']))
-    res = ' '.join(result) +'\n'
-    return res
+    return  ' '.join(result) +'\n'
 
 def print_result(result, filename=None):
     """Write memory info into file"""
     if filename:
         with open(filename, 'a') as res:
             res.write(result)
-    if not filename:
+    else:
         print result
 
 def main():
     """ Function provide information about CPU, RAM usage and processes running  """
-    args = get_optparse()                   # getting options from optpaarse
-    filename = args['filename']
-    params = sys_call(args)                 # processing selected options
+    options = get_optparse()                   # getting options from optpaarse
+    option_list = ['cpu', 'ram', 'proc']
+    filename = options['filename']
+    args = [key for key, value in options.items() if value and key in option_list]
+    if len(args) == 0:
+        args.extend(option_list)
+    params = {}
+    for cmd in args:
+        params[cmd] = sys_call(cmd)
     result = get_output(params)             # formation output
     print_result(result, filename)
 
