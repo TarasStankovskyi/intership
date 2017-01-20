be3fdd2625a4b4bdf0c8bc304db6fae6fc6e6a08
TarasStankovskyi <stankovskytaras@gmail.com>
2017-01-20 17:54:22 +0200

    refactor

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 import sys
+from collections import defaultdict
 import crawler
 import process_result as result
 import config
 
 
-conf = config.Config("/home/user1/intership/crawler.conf").config_options
-res = result.Storage(conf)
-crawler = crawler.Crawler(sys.argv[1:]).run()
-domain_ips = {}
-for values in crawler.values():
-    for val in values:
-        domain_ips.setdefault(val[1], [])
-        if val[2] not in domain_ips[val[1]]:
-            domain_ips[val[1]].append(val[2])
 
+if __name__ == '__main__':
+    conf = config.Config("/home/user1/intership/crawler.conf").config_options
+    res = result.Storage(conf)
+    crawler = crawler.Crawler(sys.argv[1:]).run()
+    domain_ips = defaultdict(set)
+    domain_mails = defaultdict(set)
+    for values in crawler.values():
+        for link, domain, ip in values:
+            domain_ips[domain].add(ip)
+            if link.startswith('mailto'):
+                domain_mails[domain].add(link.split(':')[1])
 
-domain_mails = {}
-for values in crawler.values():
-    for val in values:
-        if val[0].startswith('mailto'):
-            domain_mails.setdefault(val[1], [])
-            if val[0] not in domain_mails[val[1]]:
-                domain_mails[val[1]].append(val[0].split(':')[1])
-
-for url, values in crawler.items():
-    res.insert_url(url)
-    domains = [val[1] for val in values]
-    res.insert_domains(domains, url)
-    for domain, ips in domain_ips.items():
-        res.insert_ips(ips, domain)
-    for domain, mails in domain_mails.items():
-        res.insert_mails(mails, url, domain)
-
+    for url, values in crawler.items():
+        res.insert_url(url)
+        domains = [dom[1] for dom in values]
+        res.insert_domains(domains, url)
+        for domain, ips in domain_ips.items():
+            res.insert_ips(list(ips), domain)
+        for domain, mails in domain_mails.items():
+            res.insert_mails(list(mails), url, domain)
 
--- process_result.py
+++ process_result.py
@@ -1,96 +1,81 @@
 import MySQLdb
 
 
-class Storage(object):
+def execute(func):
+    def wrapper(self, *args):
+        func(self, *args)
+        self._db.commit()
+        self._db.rollback()
+    return wrapper
 
+
+class Storage(object):
     def __init__(self, config):
         self.__config = config
-        self.__db = MySQLdb.connect(self.__config['database']['host'],
-                                    self.__config['database']['user'],
-                                    self.__config['database']['passwd'],
-                                    self.__config['database']['db'],
-                                    int(self.__config['database']['port']))
-        self.__cursor = self.__db.cursor()
+        self._db = MySQLdb.connect(self.__config['database']['host'],
+                                   self.__config['database']['user'],
+                                   self.__config['database']['passwd'],
+                                   self.__config['database']['db'],
+                                   int(self.__config['database']['port']))
+        self.__cursor = self._db.cursor()
 
+    @execute
     def insert_url(self, url):
-        try:
-            self.__cursor.execute(
-                                  """INSERT INTO url (url, counter)
-                                  VALUES (%s, 1)
-                                  ON DUPLICATE KEY
-                                  UPDATE counter=counter+1""", [url])
-            self.__db.commit()
-        except:
-            self.__db.rollback()
-
+        self.__cursor.execute(
+                              """INSERT INTO url (url, counter)
+                              VALUES (%s, 1)
+                              ON DUPLICATE KEY
+                              UPDATE counter=counter+1""", [url])
+    @execute
     def insert_domains(self, domains, url):
+        result = []
         for domain in domains:
-            try:
-                self.__cursor.execute(
-                                       """INSERT INTO domain (domain,
-                                       counter, url)
-                                       VALUES (%s, 1, %s)
-                                       ON DUPLICATE KEY
-                                       UPDATE counter=counter+1
-                                       """, [domain, url])
-                self.__db.commit()
-                self.__insert_url_domain(url, domain)
-            except:
-                self.__db.rollback()
-
-    def __insert_url_domain(self, url, domain):
-        try:
             self.__cursor.execute(
+                                  """INSERT INTO domain (domain,
+                                  counter, url)
+                                  VALUES (%s, 1, %s)
+                                  ON DUPLICATE KEY
+                                  UPDATE counter=counter+1
+                                  """, [domain, url])
+            result.append((url, domain))
+        self.__insert_url_domain(result)
+
+    @execute
+    def __insert_url_domain(self, data):
+        self.__cursor.executemany(
                                   """INSERT IGNORE INTO url_domain (url,
                                   domain)
                                   VALUES (%s, %s)
-                                  """, [url, domain])
-            self.__db.commit()
-        except:
-            self.__db.rollback()
+                                  """, data)
+
+    @execute
     def insert_mails(self, mails, url, domain):
+        result = []
         for mail in mails:
-            try:
-                self.__cursor.execute(
-                                      """INSERT INTO mail (mail,  counter,
-                                      url, domain)
-                                      VALUES (%s, 1, %s, %s)
-                                      ON DUPLICATE KEY
-                                      UPDATE counter=counter+1
-                                      """, [mail, url, domain])
-                self.__db.commit()
-                self.__insert_url_mail(url, mail)
-            except:
-                self.__db.rollback()
-
-    def __insert_url_mail(self, url, mail):
-        try:
             self.__cursor.execute(
-                                  """INSERT IGNORE INTO url_mail (url,
-                                  mail)
-                                  VALUES (%s, %s)
-                                  """, [url, mail])
-            self.__db.commit()
-        except:
-            self.__db.rollback()
+                                  """INSERT INTO mail (mail,  counter,
+                                  url, domain)
+                                  VALUES (%s, 1, %s, %s)
+                                  ON DUPLICATE KEY
+                                  UPDATE counter=counter+1
+                                  """, [mail, url, domain])
+            result.append((url, mail))
+        self.__insert_url_mail(result)
 
+    @execute
+    def __insert_url_mail(self, data):
+        self.__cursor.executemany(
+                                  """INSERT IGNORE INTO url_mail (url, mail)
+                                  VALUES (%s, %s)
+                                  """, data)
+
+    @execute
     def insert_ips(self, ips, domain):
         for ip in ips:
-            try:
-                self.__cursor.execute(
-                                      """INSERT INTO ip (ip, counter, domain)
-                                      VALUES (%s, 1, %s)
-                                      ON DUPLICATE KEY
-                                      UPDATE counter=counter+1
-                                      """, [ip, domain])
-                self.__db.commit()
-            except:
-                self.__db.rollback()
-
-
-
-
-
-
-
+            self.__cursor.execute(
+                                  """INSERT INTO ip (ip, counter, domain)
+                                  VALUES (%s, 1, %s)
+                                  ON DUPLICATE KEY
+                                  UPDATE counter=counter+1
+                                  """, [ip, domain])
 
