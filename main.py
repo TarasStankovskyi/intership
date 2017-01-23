1f2ad4049fd583d8efb77e3c9d463cd1468a5865
TarasStankovskyi <stankovskytaras@gmail.com>
2017-01-23 14:46:52 +0200

    refactore structure

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sys
 from collections import defaultdict
 from crawler import Crawler
-from storage import Storage
-from connection import DatabaseConnection
+from storage import Storage, DatabaseConnection
 import config
 
 
@@ -12,24 +11,25 @@ if __name__ == '__main__':
     config = conf_object.config_options
     crawler = Crawler(sys.argv[1:])
     data = crawler.run()
-    with DatabaseConnection(config) as cursor:
-        storage = Storage(cursor)
+    connection =  DatabaseConnection(config)
+    storage = Storage(connection)
 
-        for url, data in data.items():
-            storage.insert_url(url)
-            domain_ips = defaultdict(set)
-            domain_mails = defaultdict(set)
-            domains = []
+    for url, data in data.items():
+        storage.insert_url(url)
+        domain_ips = defaultdict(set)
+        domain_mails = defaultdict(set)
+        domains = []
 
-            for link, domain, ip in data:
-                domains.append(domain)
-                domain_ips[domain].add(ip)
-                if link.startswith('mailto'):
-                    domain_mails[domain].add(link.split(':')[1])
+        for link, domain, ip in data:
+            domains.append(domain)
+            domain_ips[domain].add(ip)
+            if link.startswith('mailto'):
+                domain_mails[domain].add(link.split(':')[1])
 
-            storage.insert_domains(domains, url)
-            for domain, ips in domain_ips.items():
-                storage.insert_ips(list(ips), domain)
-            for domain, mails in domain_mails.items():
-                storage.insert_mails(list(mails), url, domain)
+        storage.insert_domains(domains, url)
+        for domain, ips in domain_ips.items():
+            storage.insert_ips(list(ips), domain)
+        for domain, mails in domain_mails.items():
+            storage.insert_mails(list(mails), url, domain)
+    storage.close()
 
--- storage.py
+++ storage.py
@@ -1,67 +1,102 @@
 import MySQLdb
 
+class DatabaseConnection(object):
+    def __init__(self, config):
+        self.__config = config
+        self._db = MySQLdb.connect(self.__config['database']['host'],
+                                   self.__config['database']['user'],
+                                   self.__config['database']['passwd'],
+                                   self.__config['database']['db'],
+                                   int(self.__config['database']['port']))
+    def close(self):
+        self._db.close()
+
+    def __iter__(self):
+        for item in self.__cursor:
+            yield item
+
+    def __enter__(self):
+        self.__cursor = self._db.cursor()
+        return self.__cursor
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        if isinstance(exc_value, Exception):
+            self._db.rollback()
+        else:
+            self._db.commit()
+
 
 class Storage(object):
 
-    def __init__(self, cursor):
-        self.__cursor = cursor
+    def __init__(self, connection):
+        self.__connection = connection
+
+    def close(self):
+        self.__connection.close()
+
 
     def insert_url(self, url):
-        self.__cursor.execute(
-                              """INSERT INTO url (url, counter)
-                              VALUES (%s, 1)
-                              ON DUPLICATE KEY
-                              UPDATE counter=counter+1""", [url])
+        with self.__connection as cursor:
+            cursor.execute(
+                           """INSERT INTO url (url, counter)
+                           VALUES (%s, 1)
+                           ON DUPLICATE KEY
+                           UPDATE counter=counter+1""", [url])
 
     def insert_domains(self, domains, url):
-        result = []
-        for domain in domains:
-            self.__cursor.execute(
-                                  """INSERT INTO domain (domain,
-                                  counter, url)
-                                  VALUES (%s, 1, %s)
-                                  ON DUPLICATE KEY
-                                  UPDATE counter=counter+1
-                                  """, [domain, url])
-            result.append((url, domain))
-        self.__insert_url_domain(result)
+        with self.__connection as cursor:
+            result = []
+            for domain in domains:
+                cursor.execute(
+                               """INSERT INTO domain (domain,
+                               counter, url)
+                               VALUES (%s, 1, %s)
+                               ON DUPLICATE KEY
+                               UPDATE counter=counter+1
+                               """, [domain, url])
+                result.append((url, domain))
+            self.__insert_url_domain(result)
 
 
     def __insert_url_domain(self, data):
-        self.__cursor.executemany(
-                                  """INSERT IGNORE INTO url_domain (url,
-                                  domain)
-                                  VALUES (%s, %s)
-                                  """, data)
+        with self.__connection as cursor:
+            cursor.executemany(
+                               """INSERT IGNORE INTO url_domain (url,
+                               domain)
+                               VALUES (%s, %s)
+                               """, data)
 
 
     def insert_mails(self, mails, url, domain):
-        result = []
-        for mail in mails:
-            self.__cursor.execute(
-                                  """INSERT INTO mail (mail,  counter,
-                                  url, domain)
-                                  VALUES (%s, 1, %s, %s)
-                                  ON DUPLICATE KEY
-                                  UPDATE counter=counter+1
-                                  """, [mail, url, domain])
-            result.append((url, mail))
-        self.__insert_url_mail(result)
+        with self.__connection as cursor:
+            result = []
+            for mail in mails:
+                cursor.execute(
+                               """INSERT INTO mail (mail,  counter,
+                               url, domain)
+                               VALUES (%s, 1, %s, %s)
+                               ON DUPLICATE KEY
+                               UPDATE counter=counter+1
+                               """, [mail, url, domain])
+                result.append((url, mail))
+            self.__insert_url_mail(result)
 
 
     def __insert_url_mail(self, data):
-        self.__cursor.executemany(
-                                  """INSERT IGNORE INTO url_mail (url, mail)
-                                  VALUES (%s, %s)
-                                  """, data)
+        with self.__connection as cursor:
+            cursor.executemany(
+                               """INSERT IGNORE INTO url_mail (url, mail)
+                               VALUES (%s, %s)
+                               """, data)
 
 
     def insert_ips(self, ips, domain):
-        for ip in ips:
-            self.__cursor.execute(
-                                  """INSERT INTO ip (ip, counter, domain)
-                                  VALUES (%s, 1, %s)
-                                  ON DUPLICATE KEY
-                                  UPDATE counter=counter+1
-                                  """, [ip, domain])
+        with self.__connection as cursor:
+            for ip in ips:
+                cursor.execute(
+                               """INSERT INTO ip (ip, counter, domain)
+                               VALUES (%s, 1, %s)
+                               ON DUPLICATE KEY
+                               UPDATE counter=counter+1
+                               """, [ip, domain])
 
