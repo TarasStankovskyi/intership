19961ec8d737ef9203b3315cbdff3d6db8da6919
TarasStankovskyi <stankovskytaras@gmail.com>
2017-01-26 12:41:17 +0200

    refactor

--- main.py
+++ main.py
@@ -1,42 +1,7 @@
-import sys
-import socket
-from collections import defaultdict
-from crawler import Crawler
-from storage import Storage, DatabaseConnection
-import config
 from server import Server
 
 
 if __name__ == '__main__':
 
     server = Server()
-    conn = server.connection()
-    arguments = server.arguments
-
-    conf_object = config.Config("/home/user1/intership/crawler.conf")
-    config = conf_object.config_options
-    crawler = Crawler(arguments)
-
-    data = crawler.run()
-    connection =  DatabaseConnection(config)
-    storage = Storage(connection)
-
-    for url, data in data.items():
-        storage.insert_url(url)
-        domain_ips = defaultdict(set)
-        domain_mails = defaultdict(set)
-        domains = []
-
-        for link, domain, ip in data:
-            domains.append(domain)
-            domain_ips[domain].add(ip)
-            if link.startswith('mailto'):
-                domain_mails[domain].add(link.split(':')[1])
-
-        storage.insert_domains(domains, url)
-        for domain, ips in domain_ips.items():
-            storage.insert_ips(list(ips), domain)
-        for domain, mails in domain_mails.items():
-            storage.insert_mails(list(mails), url, domain)
-    storage.close()
-    print 'Data has been saved'
+    conn = server.run()
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import socket
+from collections import defaultdict
+from crawler import Crawler
+from storage import Storage, DatabaseConnection
+import config
 
 class Server(object):
 
@@ -7,7 +11,7 @@ class Server(object):
         self.port = port
         self.que = que
 
-    def connection(self):
+    def __connection(self):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.bind(('', self.port))
@@ -24,6 +28,36 @@ class Server(object):
 
         finally:
             sock.close()
+
+    def run(self):
+        self.__connection()
+        conf_object = config.Config("/home/user1/intership/crawler.conf")
+        conf = conf_object.config_options
+        crawler = Crawler(self.arguments)
+
+        data = crawler.run()
+        connection = DatabaseConnection(conf)
+        storage = Storage(connection)
+
+        for url, data in data.items():
+            storage.insert_url(url)
+            domain_ips = defaultdict(set)
+            domain_mails = defaultdict(set)
+            domains = []
+
+            for link, domain, ip in data:
+                domains.append(domain)
+                domain_ips[domain].add(ip)
+                if link.startswith('mailto'):
+                    domain_mails[domain].add(link.split(':')[1])
+
+            storage.insert_domains(domains, url)
+            for domain, ips in domain_ips.items():
+                storage.insert_ips(ips, domain)
+            for domain, mails in domain_mails.items():
+                storage.insert_mails(mails, url, domain)
+        storage.close()
+        print 'Data has been saved'
 
 
 if  __name__ == '__main__':
