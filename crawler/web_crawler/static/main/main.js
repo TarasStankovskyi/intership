5e3a48f9b3d776aecb75957f0173b46bc6d94fa0
TarasStankovskyi <stankovskytaras@gmail.com>
2017-03-28 15:49:08 +0300

    add skeleton of project

---
+++ crawler/web_crawler/static/main/main.js
@@ -0,0 +1,57 @@
+
+function showTabs(elem) {
+    var tabcontent = document.getElementsByClassName("tab_content")
+    for  ( var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("current");
+    }
+
+    var tab = elem.getAttribute("data-tab");
+    document.getElementById(tab).classList.add("current");
+
+    var tablinks = document.getElementsByClassName("tablink");
+    for (i=0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("selected");
+    }
+    elem.classList.add("selected");
+
+};
+
+
+function get_modal(id) {
+    var url = document.getElementById(id).innerText;
+    var modal =  document.getElementById("my_modal").style.display = "block";
+    document.getElementById("my_input").value = url;
+    var id_field = document.getElementById("source_id");
+    id_field.value = id.slice(4)
+};
+
+function editClose() {
+    document.getElementById("my_modal").style.display = "none";
+};
+
+function editUrl() {
+    var url = document.getElementById("my_input").value;
+    var url_id = document.getElementById("source_id").value;
+    document.getElementById("url_" + url_id).innerHTML=url;
+    editClose();
+
+};
+
+function status_button(elem) {
+    var class_list = elem.classList;
+    if (class_list.contains("status_btn_on")) {
+        class_list.remove("status_btn_on");
+        class_list.add("status_btn_off");
+    } else {
+        class_list.remove("status_btn_off");
+        class_list.add("status_btn_on");
+    }
+};
+
+
+
+
+
+
+
+
