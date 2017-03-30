be5d0d3f349a64e847ae7c5d8cd01d5548be6270
TarasStankovskyi <stankovskytaras@gmail>
2017-03-30 16:53:02 +0300

    add static

--- crawler/project/project/settings.py
+++ crawler/project/project/settings.py
@@ -51,22 +51,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'project.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
@@ -119,12 +103,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'project/static'),
+        os.path.join(BASE_DIR, 'project/static/'),
         )
 TEMPLATES = [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': ['templates', 'project/templates'],
+            'DIRS': [os.path.join(BASE_DIR, 'project/templates')],
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
